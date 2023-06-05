e18f81fd8011cb70e8ef4746f782714810bc38ca
Scott Wu <scott.wu.hjx@gmail.com>
2023-06-04 20:19:09 -0700

    MVP

---
+++ src/App.tsx
@@ -0,0 +1,164 @@
+import { CronJob } from "cron";
+import { List } from "immutable";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+
+import styles from "./App.module.scss";
+
+interface Site {
+  id: number;
+  cron: string;
+  url: string;
+  running: boolean;
+}
+
+interface Runner {
+  cron: string;
+  url: string;
+  job: CronJob | null;
+}
+
+function refresh(handles: Map<number, Window>, site: Site) {
+  handles.set(site.id, window.open(site.url, site.id.toString())!);
+}
+
+function makeCron(handles: Map<number, Window>, site: Site) {
+  try {
+    return new CronJob(site.cron, () => refresh(handles, site), null, false);
+  } catch (e) {
+    return null;
+  }
+}
+
+function Sites(props: {
+  sites: List<Site>;
+  onDelete: (index: number) => void;
+  onUpdate: (k: Iterable<unknown>, v: unknown) => void;
+}) {
+  const elements = useMemo(() => {
+    return props.sites.map((site, index) => {
+      return (
+        <div key={index} className={styles.site}>
+          <button
+            className={styles.delete}
+            onClick={() => props.onDelete(index)}
+          >
+            &times;
+          </button>
+          <input
+            className={styles.cron}
+            type="text"
+            value={site.cron}
+            onChange={(event) =>
+              props.onUpdate([index, "cron"], event.target.value)
+            }
+            readOnly={site.running}
+          />
+          <input
+            className={styles.url}
+            type="text"
+            value={site.url}
+            onChange={(event) =>
+              props.onUpdate([index, "url"], event.target.value)
+            }
+            readOnly={site.running}
+          />
+          <input
+            className={styles.running}
+            type="checkbox"
+            checked={site.running}
+            onChange={(event) =>
+              props.onUpdate([index, "running"], event.target.checked)
+            }
+          />
+        </div>
+      );
+    });
+  }, [props.onDelete, props.onUpdate, props.sites]);
+
+  return <div className={styles.sites}>{elements}</div>;
+}
+
+function AddButton(props: { onAdd: () => void }) {
+  return (
+    <button className={styles.add} onClick={props.onAdd}>
+      Add Site
+    </button>
+  );
+}
+
+export default function App() {
+  const [sites, setSites] = useState<List<Site>>(List());
+  const runners = useRef<Map<number, Runner>>(new Map());
+  const handles = useRef<Map<number, Window>>(new Map());
+
+  const onAdd = useCallback(() => {
+    setSites((prevSites) =>
+      prevSites.push({
+        id: Date.now(),
+        cron: "0 * * * * *",
+        url: "https://reddit.com",
+        running: false,
+      })
+    );
+  }, []);
+
+  const onDelete = useCallback((index: number) => {
+    setSites((prevSites) => prevSites.delete(index));
+  }, []);
+
+  const onUpdate = useCallback((k: Iterable<unknown>, v: unknown) => {
+    setSites((prevSites) => prevSites.setIn(k, v));
+  }, []);
+
+  useEffect(() => {
+    // Delete any old runners
+    for (const [k, v] of runners.current.entries()) {
+      if (sites.find((s) => s.id === k) == null) {
+        v.job?.stop();
+        runners.current.delete(k);
+
+        const h = handles.current.get(k);
+        if (h != null) {
+          h.close();
+          handles.current.delete(k);
+        }
+      }
+    }
+
+    // Create or update runners
+    for (const s of sites) {
+      let r = runners.current.get(s.id);
+      if (r != null && (r.cron !== s.cron || r.url !== s.url)) {
+        if (r.job != null) {
+          r.job.stop();
+        }
+        r.job = makeCron(handles.current, s);
+      } else if (r == null) {
+        r = {
+          cron: s.cron,
+          url: s.url,
+          job: makeCron(handles.current, s),
+        };
+        runners.current.set(s.id, r);
+      }
+
+      // Start or stop jobs
+      if (r.job != null) {
+        if (r.job.running && !s.running) {
+          r.job.stop();
+        } else if (!r.job.running && s.running) {
+          r.job.start();
+        }
+      }
+    }
+  }, [sites]);
+
+  return (
+    <div className={styles.app}>
+      <h1>Cron Refresher</h1>
+      <hr />
+      <Sites sites={sites} onDelete={onDelete} onUpdate={onUpdate} />
+      <AddButton onAdd={onAdd} />
+    </div>
+  );
+}
