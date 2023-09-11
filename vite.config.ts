bed438c38a4908c86ad3f604298789a1522adf51
Scott Wu <scott.wu.hjx@gmail.com>
2023-09-10 18:27:30 -0700

    Add ws page to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         once: resolve(__dirname, "once/index.html"),
+        ws: resolve(__dirname, "ws/index.html"),
       },
     },
   },
